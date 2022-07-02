d4683828517ff9d0d2fc8a855633364fded06e2b
rashidgafurov <nsd7c4@gmail.com>
2022-07-02 18:26:14 -0400

    Initial commit

---
+++ src/main/java/com/grr/jd213springmockito/Jd213SpringMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.grr.jd213springmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jd213SpringMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jd213SpringMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grr/jd213springmockito/Jd213SpringMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grr.jd213springmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jd213SpringMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
